db1a5b7617e53f01a37228bb9cecc39918c7c8e2
Shamseera <kurumbeths@gmail.com>
2020-06-15 18:24:26 +1000

    remove spacing between codes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 const catFactsUrl = "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1" ;
 
-
 function getcatFacts() {
     fetch(catFactsUrl)
         .then((response) => response.json())
@@ -10,18 +9,13 @@ function getcatFacts() {
 }
 
 function showCatFacts(data) {
-   
-
     let factsDiv = document.getElementById("facts");
     factsDiv.innerHTML = null;
     let factP = document.createElement("p");
     factP.textContent = data.text;
     factP.style.fontSize = "25px";
-    factsDiv.appendChild(factP);
-  
-}  
-
-    
+    factsDiv.appendChild(factP); 
+}     
 document.getElementById("submit-name").addEventListener("click",getcatFacts)
 
 
